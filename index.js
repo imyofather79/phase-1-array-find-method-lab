06201ec3796b340816569dd950280ac00df34a77
imyofather79 <chunsum708@gmail.com>
2021-05-30 01:37:50 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 
+
+function superbowlWin(array){
+
+    const winYear = array.find(record => record.result === "W");
+    
+    if (!!winYear){
+        return winYear.year;
+    } else {
+        return undefined;
+    }
+    
+}
